b1b1805533bda18b02f6ebe738135b59510c6940
alencar-felipe <alencar.felipe.paiva@gmail.com>
2021-09-01 15:46:58 -0300

    unnecessary import

--- src/test/java/com/alencarfelipe/mytwitter/TwitterTest.java
+++ src/test/java/com/alencarfelipe/mytwitter/TwitterTest.java
@@ -3,8 +3,6 @@ package com.alencarfelipe.mytwitter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
